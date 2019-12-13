2c5c19ebba977ddfa61103eaaa5dbaab3741d68f
Bigshmow <dstewart88@gmail.com>
2019-12-13 13:37:09 -0800

    Changed title to my portfolio

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.Public_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
